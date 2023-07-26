8001a8fb0f9c54c2f5c0572cdb96cde4a26227f1
Aryan Singh <aryansunny285@gmail.com>
2023-07-26 10:26:25 +0530

    c

---
+++ Problems/50A - Domino piling.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+
+    int a,b;
+    cin>>a>>b;
+
+    int sum=a*b/2;
+    cout<<sum<<endl;
+    return 0;
+}
